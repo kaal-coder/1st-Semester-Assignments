c146930ea7f052aff6cf6b330c03a1218316cc59
Kaushik Lakhani <kaushik.lakhani123@gmail.com>
2022-01-23 16:36:45 +0530

    Update Question03.java

--- labAssignment01/Question03.java
+++ labAssignment01/Question03.java
@@ -1,11 +1,3 @@
-/* Author's name		:Kaushik Lakhani	
- * Author's Regd.No		:204102002
- * Author's Semester	:1st
- * Author's Branch		:CSIT(2041010)
- * Subject				:ICP (CSE 1001)
- * Problem Description	(lab assignment,Q03)Write a java program to display your initials on the screen in block letters
- * as shown. For example the name Tapan Kumar
- */
 package labAssignment01;
 
 public class Question03 {
