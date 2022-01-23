7b6e4a1b0db2cb42a13a15539629d153fa45d9a2
Kaushik Lakhani <kaushik.lakhani123@gmail.com>
2022-01-23 16:36:17 +0530

    Update Question02.java

--- labAssignment01/Question02.java
+++ labAssignment01/Question02.java
@@ -1,21 +1,3 @@
-/* Author's name		:Kaushik Lakhani	
- * Author's Regd.No		:204102002
- * Author's Semester	:1st
- * Author's Branch		:CSIT(2041010)
- * Subject				:ICP (CSE 1001)
- * Problem Description	(lab assignment,Q02)Write a java program that displays your name and address on the screen as if
-it were a letter. Your output should look something like that below.
-+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−+
-| #### |
-| #### |
-| #### |
-| |
-| |
-| Biswanath Dash |
-| H.N: 1 0 9 , Baramunda |
-| Bhubaneswar |
-+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−+
- */
 package labAssignment01;
 
 public class Question02 {
