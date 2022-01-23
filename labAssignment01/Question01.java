174a60652029e3de3e8b94fccec34e8ee00881f8
Kaushik Lakhani <kaushik.lakhani123@gmail.com>
2022-01-23 16:35:35 +0530

    Update Question01.java

--- labAssignment01/Question01.java
+++ labAssignment01/Question01.java
@@ -1,17 +1,3 @@
-/* Author's name		:Kaushik Lakhani	
- * Author's Regd.No		:204102002
- * Author's Semester	:1st
- * Author's Branch		:CSIT(2041010)
- * Subject				:ICP (CSE 1001)
- * Problem Description	(lab assignment,Q01)Write a java program to display following messages.
-He l l o World !
-He l l o Again
-I l i k e typing t h i s .
-This i s fun .
-Yay ! Pr int ing .
-I ’ d much r a the r you ’ not ’ .
-I " s a id " do not touch t h i s .
- */
 package labAssignment01;
 
 public class Question01 {
