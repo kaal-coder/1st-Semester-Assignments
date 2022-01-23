d86800d83136c5ba98246fcdced0469721cd8e7c
Kaushik Lakhani <kaushik.lakhani123@gmail.com>
2022-01-23 16:37:41 +0530

    Update Question05.java

--- labAssignment01/Question05.java
+++ labAssignment01/Question05.java
@@ -1,13 +1,3 @@
-/* Author's name		:Kaushik Lakhani	
- * Author's Regd.No		:204102002
- * Author's Semester	:1st
- * Author's Branch		:CSIT(2041010)
- * Subject				:ICP (CSE 1001)
- * Problem Description	(lab assignment,Q05)Write a java program that stores your Regd. No and year of admission into two
-variables, and displays their values on the screen.
-My Regd . No i s 2040102002 and I have taken admission in
-B. Tech . In 2020.
- */
 package labAssignment01;
 
 public class Question05 {
